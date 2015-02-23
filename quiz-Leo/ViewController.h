f78d8773139c93b503be4a0c084a8e91d4c09a28
LeonardoBrunassi <leo.brunassi.apple@hotmail.com>
2015-02-23 19:32:31 -0300

    Initial Commit

---
+++ quiz-Leo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  quiz-Leo
+//
+//  Created by Leonardo Rodrigues de Morais Brunassi on 23/02/15.
+//  Copyright (c) 2015 Leonardo Rodrigues de Morais Brunassi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ quiz-Leo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  quiz-Leo
+//
+//  Created by Leonardo Rodrigues de Morais Brunassi on 23/02/15.
+//  Copyright (c) 2015 Leonardo Rodrigues de Morais Brunassi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
